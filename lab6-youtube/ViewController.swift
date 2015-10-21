784e76c21342413656494bcc4608086d51282dff
Yosemite <Yosemite@yosemites-macbook-pro.local>
2015-10-21 11:11:30 -0700

    Initial Commit

---
+++ lab6-youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab6-youtube
+//
+//  Created by Yosemite on 10/21/15.
+//  Copyright © 2015 Yosemite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
